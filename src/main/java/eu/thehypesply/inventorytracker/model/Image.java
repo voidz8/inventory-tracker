bdf00036644ebd79fd14d0a5ccb0912314cad61c
voidz8 <renzovermue@hotmail.com>
2021-04-08 14:06:53 +0200

    updated image model

--- src/main/java/eu/thehypesply/inventorytracker/model/Image.java
+++ src/main/java/eu/thehypesply/inventorytracker/model/Image.java
@@ -1,13 +1,24 @@
 package eu.thehypesply.inventorytracker.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document
+@Data
 public class Image {
 
     @Id
     private String id;
 
-    private String pid;
+    @Field
+    private String fileName;
+
+    @Field
+    private String fileType;
+
+    @Field
+    private byte[] data;
+
 }
